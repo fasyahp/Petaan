b8ce7279103a43dcd09b557451428f75220b4c5e
eavelie <athallahnaufalr@gmail.com>
2024-11-07 19:27:31 +0700

    added some changes

--- app/src/main/java/com/kelompok2/petaan/AddROIFragment.kt
+++ app/src/main/java/com/kelompok2/petaan/AddROIFragment.kt
@@ -6,13 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.ViewGroup.MarginLayoutParams
+import android.view.WindowManager
 import android.widget.Toast
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.updateLayoutParams
+import androidx.compose.ui.text.toLowerCase
 import androidx.navigation.findNavController
 import com.google.firebase.Firebase
+import com.google.firebase.firestore.GeoPoint
 import com.google.firebase.firestore.firestore
 import com.kelompok2.petaan.databinding.FragmentAddroiBinding
 
@@ -32,6 +31,8 @@ class AddROIFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        requireActivity().window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN)
+//        binding!!.locationTextField.text =
 /*
         ViewCompat.setOnApplyWindowInsetsListener(binding!!.addROIAppBarContainer) { v, windowInsets ->
             val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -51,12 +52,21 @@ class AddROIFragment : Fragment() {
 */
 
         binding!!.saveAddReportButton.setOnClickListener { v ->
+            val severity = when (binding!!.severityDropDown.text.toString()) {
+                "Low" -> 1
+                "Moderate" -> 2
+                "High" -> 3
+                "Critical" -> 4
+                "Emergency" -> 5
+                else -> -1
+            }
+            val latLng = binding!!.locationTextField.text?.split(",")
             val db = Firebase.firestore
             val report = hashMapOf(
                 "subject" to binding!!.subjectTextField.text.toString(),
                 "description" to binding!!.descriptionTextField.text.toString(),
-                "severity" to binding!!.severityDropDown.text.toString(),
-                "location" to binding!!.locationTextField.text.toString()
+                "severity" to severity,
+                "location" to GeoPoint(latLng!![0].toDouble(), latLng[1].toDouble())
             )
             db.collection("reports")
                 .add(report)
