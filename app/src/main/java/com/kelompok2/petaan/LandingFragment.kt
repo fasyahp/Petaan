195c65efad9430a742e924d108026a7046c1e4df
Sadam <msadamramli@gmail.com>
2024-11-12 19:22:26 +0700

    menambah icon mata pada password edit text dan login session

--- app/src/main/java/com/kelompok2/petaan/LandingFragment.kt
+++ app/src/main/java/com/kelompok2/petaan/LandingFragment.kt
@@ -1,17 +1,21 @@
 package com.kelompok2.petaan
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.findNavController
+import com.google.firebase.auth.FirebaseAuth
 import com.kelompok2.petaan.databinding.FragmentLandingBinding
 import com.kelompok2.petaan.databinding.FragmentSearchBinding
 
 class LandingFragment : Fragment() {
 
     private var binding: FragmentLandingBinding? = null
+    private lateinit var firebaseAuth: FirebaseAuth
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,12 +29,26 @@ class LandingFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding!!.landingLoginButton.setOnClickListener {
-            //fungsi navigate to fragment yang ada di kelas AuthActivity
-            (activity as AuthActivity).navigateToFragment(LoginFragment())
-        }
-        binding!!.landingRegisterButton.setOnClickListener {
-            (activity as AuthActivity).navigateToFragment(RegistrationFragment())
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        val currentUser = firebaseAuth.currentUser
+        if (currentUser != null) {
+            // User is already logged in, navigate to MainActivity
+            startActivity(
+                Intent(requireContext(), MainActivity::class.java).apply {
+                    flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                }
+            )
+            requireActivity().finish()
+        } else {
+            // User is not logged in, show login/register options
+            binding?.landingLoginButton?.setOnClickListener {
+                (activity as AuthActivity).navigateToFragment(LoginFragment())
+            }
+            binding?.landingRegisterButton?.setOnClickListener {
+                (activity as AuthActivity).navigateToFragment(RegistrationFragment())
+            }
         }
     }
 }
--- app/src/main/java/com/kelompok2/petaan/LoginFragment.kt
+++ app/src/main/java/com/kelompok2/petaan/LoginFragment.kt
@@ -2,10 +2,14 @@ package com.kelompok2.petaan
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ImageView
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.kelompok2.petaan.databinding.FragmentLoginBinding
@@ -14,6 +18,7 @@ class LoginFragment : Fragment() {
 
     private var binding: FragmentLoginBinding? = null
     private lateinit var firebaseAuth: FirebaseAuth
+    private var isPasswordVisible = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,13 +36,32 @@ class LoginFragment : Fragment() {
         firebaseAuth = FirebaseAuth.getInstance()
         val binding = binding ?: return // biar binding ga null
 
+        // Set initial password transformation
+        binding.editPassword.transformationMethod = PasswordTransformationMethod.getInstance()
 
-        binding!!.loginButton.setOnClickListener() {
+        // Setup password toggle
+        binding.passwordToggle.setOnClickListener {
+            isPasswordVisible = !isPasswordVisible
+
+            // Toggle password visibility
+            if (isPasswordVisible) {
+                // Show password
+                binding.editPassword.transformationMethod = HideReturnsTransformationMethod.getInstance()
+                binding.passwordToggle.setImageResource(R.drawable.ic_eye_open)
+            } else {
+                // Hide password
+                binding.editPassword.transformationMethod = PasswordTransformationMethod.getInstance()
+                binding.passwordToggle.setImageResource(R.drawable.ic_eye_close)
+            }
+            // Maintain cursor position
+            binding.editPassword.setSelection(binding.editPassword.text.length)
+        }
+
+        binding.loginButton.setOnClickListener {
             val email = binding.editEmail.text.toString()
             val password = binding.editPassword.text.toString()
 
             if (email.isNotEmpty() && password.isNotEmpty()) {
-
                 firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
                     if (it.isSuccessful) {
                         startActivity(
@@ -50,8 +74,6 @@ class LoginFragment : Fragment() {
                         Toast.makeText(requireContext(), it.exception?.message, Toast.LENGTH_SHORT)
                             .show()
                     }
-
-
                 }
             }
         }
